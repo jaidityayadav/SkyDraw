688ae5f28dc15a7d3325268f9cb4c0a02819c3e0
Jaiditya Yadav <jaiditya2003@gmail.com>
2025-05-12 02:05:27 +0530

    to fix

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,30 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import { Jwt } from "jsonwebtoken";
+import { userSignupSchema } from "@repo/validators"
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/signup", (req: Request, res: Response) => {
+    const result = userSignupSchema.safeParse(req.body);
+
+    if (!result.success) {
+        return res.status(400).json({ message: "Invalid input" });
+    }
+
+    // Proceed with valid data
+    res.json({ message: "success" });
+
+})
+
+app.post("/login", (req:Request, res:Response) => {
+
+})
+
+app.post("/room", (req:Request, res:Response) => {
+
+})
+
 
 app.listen(3001);
