eeba69000fbd509df88f27ebd204e6d4ae9a1bb8
Jaiditya Yadav <jaiditya2003@gmail.com>
2025-05-13 17:40:15 +0530

    update whole reppo

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,13 +1,28 @@
+import path from "path";
 import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { jwtsecret } from '@repo/backend-common';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+wss.on('connection', function connection(ws, Request) {
+  const url = Request.url;
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+
+  const token = queryParams.get('token') || "";
+  const decoded = jwt.verify(token, jwtsecret)
+
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    ws.send('pong');
   });
 
-  ws.send('something');
 });
---
+++ packages/validators/src/index.d.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+export declare const userSignupSchema: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+//# sourceMappingURL=index.d.ts.map
